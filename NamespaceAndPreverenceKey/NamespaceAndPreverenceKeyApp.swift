66cd92e973542080f595b4c82ce83ca68fc4dd60
ihValery <ihValery@gmail.com>
2022-05-05 22:10:31 +0300

    Initial Commit

---
+++ NamespaceAndPreverenceKey/NamespaceAndPreverenceKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NamespaceAndPreverenceKeyApp.swift
+//  NamespaceAndPreverenceKey
+//
+//  Created by Валерий Игнатьев on 5.05.22.
+//
+
+import SwiftUI
+
+@main
+struct NamespaceAndPreverenceKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
